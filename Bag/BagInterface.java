a99f90dbd78281daaffaf2c9726cbda514a723bc
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-03-20 09:20:10 +0800

    Update BagInterface.java

--- Bag/BagInterface.java
+++ Bag/BagInterface.java
@@ -1,4 +1,13 @@
-package week3.T;
+/**This is the interface for the Bag Data Structure.
+  *The idea is simple:
+  *Imagine there's a bag (the structure), we can put anything inside the bag, which we call as the items (data).
+  *The order of the items is insignificant and it is possible to have more than one similar item.
+  *Therefore, the structure is not a set. However, I assumed that the bag is in a form of array.
+  *Below are the 'features' needed for the bag data structure.
+  *Reference: https://algs4.cs.princeton.edu/13stacks/
+  
+  NOTE: I may make some corrections for this source code.
+**/
 
 public interface BagInterface <T>{
 
