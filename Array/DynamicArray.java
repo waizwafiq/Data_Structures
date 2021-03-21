ac2e4ccc4aa4e511854ee4b18df065f10384af00
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-03-21 17:04:31 +0800

    Update DynamicArray.java

--- Array/DynamicArray.java
+++ Array/DynamicArray.java
@@ -1,62 +1,85 @@
-public class DynamicArray<T> implements DynamicArrayInterface<T>{
-    
+import java.util.Random;
+
+public class DynamicArray<T> implements DynamicArrayInterface<T> {
+
     private T[] arr;
     private int len;
 
-    public DynamicArray(T[] arr){
+    public DynamicArray(T[] arr) {
         this.arr = arr;
         this.len = arr.length;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return arr.length == 0;
     }
 
-    public void display(){
+    public void display() {
         System.out.print("[ ");
-        for(T elems: arr) System.out.print(elems+" ");
+        for (T elems : arr)
+            System.out.print(elems + " ");
         System.out.print("]");
     }
-    
-    public boolean search(T key){
-        for(int i=0; i<len; i++)
-            if(arr[i].equals(key)) return true;
+
+    public boolean search(T key) {
+        for (int i = 0; i < len; i++)
+            if (arr[i].equals(key))
+                return true;
         return false;
     }
-    
-    public void add(T in){
-        T[] temp = (T[]) new Object[len+1];
-        
-        for(int i=0; i<len; i++) temp[i] = this.arr[i];
+
+    public void add(T in) {
+        T[] temp = (T[]) new Object[len + 1];
+
+        for (int i = 0; i < len; i++)
+            temp[i] = this.arr[i];
         temp[len] = in;
         this.arr = temp;
     }
 
-    public boolean remove(T toRemove){
-        if(!isEmpty() && search(toRemove)){
+    public boolean add(T in, int idx) {
+        if (idx >= len + 1)
+            return false;
+        else {
+            T[] temp = (T[]) new Object[len + 1];
+            for (int i = 0; i < idx; i++)
+                temp[i] = arr[i];
+            temp[idx] = in;
+            for (int i = idx + 1; i < len; i++)
+                temp[i] = arr[i];
+
+            this.arr = temp;
+            return true;
+        }
+    }
+
+    public boolean remove() {
+        if (!isEmpty()) {
+            int randIdx = new Random().nextInt(len); // get a random index
+
             T[] temp = (T[]) new Object[len - 1];
 
-            for(int i=0,j=0; i<len; i++)
-                if (!arr[i].equals(toRemove)) temp[j++] = arr[i];
-            arr = temp;
+            for (int i = 0, j = 0; i < len; i++)
+                if (!arr[i].equals(arr[randIdx]))
+                    temp[j++] = arr[i];
+
+            this.arr = temp;
             return true;
-        }else return false;
+        } else
+            return false;
     }
 
-    
+    public boolean remove(T toRemove) {
+        if (!isEmpty() && search(toRemove)) {
+            T[] temp = (T[]) new Object[len - 1];
 
-}
-
-class test{
-    public static void main(String[] args) {
-        DynamicArray<Integer> arr = new DynamicArray<>(new Integer[] {3,2,3,4,5});
-        
-        arr.add(3);
-
-
-        arr.display();
-
-
-
+            for (int i = 0, j = 0; i < len; i++)
+                if (!arr[i].equals(toRemove))
+                    temp[j++] = arr[i];
+            this.arr = temp;
+            return true;
+        } else
+            return false;
     }
+
 }
