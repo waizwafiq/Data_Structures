6fb6a934f7433b37d7d9df15896146e861d3f923
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-03-21 21:37:06 +0800

    Added Exception Handling
    if #items in bag > MAX_CAPACITY: throw exception
    else: run the code

--- Bag/Bag.java
+++ Bag/Bag.java
@@ -1,79 +1,115 @@
 import java.util.Random;
-public class Bag<T> implements BagInterface<T>{
-    T[] bag;
+
+public class Bag<T> implements BagInterface<T> {
+    private T[] bag;
+    private static final int MAX_CAPACITY = 1000;
 
     public Bag() {
         bag = (T[]) new Object[0];
     }
 
     public Bag(T[] bag) {
-        this.bag = bag;
+        if (bag.length <= MAX_CAPACITY)
+            this.bag = bag;
+        else
+            throw new ExceptionInInitializerError("The bag reached its maximum capacity.");
     }
 
-    public int getCurrentSize(){
+    public int getCurrentSize() {
         return bag.length;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return bag.length == 0;
     }
 
-    public boolean add(T newItem){
-        T[] temp = (T[]) new Object[getCurrentSize() + 1];
-        for (int i = 0; i < getCurrentSize(); i++) temp[i] = bag[i];
-        temp[getCurrentSize()] = newItem;
-        bag = temp;
-        return true;
+    public boolean add(T newItem) {
+        if (bag.length <= MAX_CAPACITY) {
+            T[] temp = (T[]) new Object[getCurrentSize() + 1];
+            for (int i = 0; i < getCurrentSize(); i++)
+                temp[i] = bag[i];
+            temp[getCurrentSize()] = newItem;
+            bag = temp;
+            return true;
+        } else
+            return false;
+
     }
 
-    public T remove(){
-        Random rand = new Random();
-        if(!isEmpty()){
-            T toRemove = bag[rand.nextInt(getCurrentSize())];
+    public T remove() {
+        if (!isEmpty()) {
+            T toRemove = bag[new Random().nextInt(getCurrentSize())];
 
-            T[] temp = (T[]) new Object[getCurrentSize()-1];
-            for(int i=0,j=0; i<getCurrentSize(); i++)
-                if(bag[i]!=toRemove) temp[j++] = bag[i];
+            T[] temp = (T[]) new Object[getCurrentSize() - 1];
+            for (int i = 0, j = 0; i < getCurrentSize(); i++)
+                if (bag[i] != toRemove)
+                    temp[j++] = bag[i];
 
             bag = temp;
             return toRemove;
-        }else return null;
+        } else
+            return null;
     }
 
-    public boolean remove(T toRemove){
-        if(!isEmpty()){
-            T[] temp = (T[]) new Object[getCurrentSize()-1];
-            for(int i=0,j=0; i<getCurrentSize(); i++)
-                if(bag[i]!=toRemove) temp[j++] = bag[i];
+    public boolean remove(T toRemove) {
+        if (!isEmpty() && contains(toRemove)) {
+            T[] temp = (T[]) new Object[getCurrentSize() - 1];
+            for (int i = 0, j = 0; i < getCurrentSize(); i++)
+                if (bag[i] != toRemove)
+                    temp[j++] = bag[i];
 
             bag = temp;
             return true;
-        }else return false;
+        } else
+            return false;
     }
 
     public void clear() {
         bag = (T[]) new Object[0];
     }
 
-    public int getFrequencyOf(T item){
-        if(!isEmpty()){
+    public int getFrequencyOf(T item) {
+        if (!isEmpty() && contains(item)) {
             int count = 0;
-            for(T items:bag)
-                if(items.equals(item)) count++;
+            for (T items : bag)
+                if (items.equals(item))
+                    count++;
 
             return count;
-        }else return 0;
+        } else
+            return 0;
     }
 
-    public boolean contains(T item){
-        if(!isEmpty())
+    public boolean contains(T item) {
+        if (!isEmpty())
             for (T items : bag)
-                if(items.equals(item)) return true;
+                if (items.equals(item))
+                    return true;
 
-        return false; //if not found
+        return false; // if not found
+
+        // alternative: return (!isEmpty() && getIndexOf(item) != -1);
     }
 
-    public T[] toArray(){
+    public T[] toArray() {
         return bag;
+    }
+
+    @Override
+    public BagInterface<T> union(BagInterface<T> anotherBag) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public BagInterface<T> intersection(BagInterface<T> anotherBag) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public BagInterface<T> difference(BagInterface<T> anotherBag) {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
