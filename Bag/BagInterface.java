0744f7a8382651586c0985424bbb73093bc61b9b
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-03-22 00:05:51 +0800

    Updated documentation

--- Bag/BagInterface.java
+++ Bag/BagInterface.java
@@ -1,12 +1,12 @@
 /**This is the interface for the Bag Data Structure.
-  *The idea is simple:
-  *Imagine there's a bag (the structure), we can put anything inside the bag, which we call as the items (data).
-  *The order of the items is insignificant and it is possible to have more than one similar item.
-  *Therefore, the structure is not a set. However, I assumed that the bag is in a form of array.
-  *Below are the 'features' needed for the bag data structure.
-  *Reference: https://algs4.cs.princeton.edu/13stacks/
+  *<p>Imagine there's a bag (the structure), we can put anything inside the bag, which we call as the items (data). 
+  *The order of the items is insignificant and it is possible to have more than one similar item. 
+  *Therefore, the structure is not a set. However, I assumed that the bag is in a form of array. 
+  *<p>Reference: https://algs4.cs.princeton.edu/13stacks/
   
-  NOTE: I may make some corrections for this source code.
+  <p>NOTE: I may make some corrections for this source code.
+
+  @author Waiz Wafiq
 */
 
 public interface BagInterface <T>{
@@ -18,18 +18,14 @@ public interface BagInterface <T>{
 
     /**Check if the bag is empty.
      *
-     * @return:
-     * true : The bag is empty
-     * false: The bag is not empty,
+     * @return true : The bag is empty
      */
     public boolean isEmpty();
 
     /**Add an item into the bag.
      * 
      * @param newItem : the item to be added into the bag
-     * @return: 
-     * true : Adding successful
-     * false: Adding failed   
+     * @return true : Adding successful
      */
     public boolean add(T newItem);
 
@@ -42,7 +38,7 @@ public interface BagInterface <T>{
     /**Removes a specified item from the bag
      * 
      * @param item : the item to be removed from the bag
-     * @return: The item removed from the bag
+     * @return: true: item removal successful
     */
     public boolean remove(T item);
 
@@ -52,22 +48,20 @@ public interface BagInterface <T>{
     /**Get the number of occurrences of the item in the bag
      * 
      * @param item : The specified item in the bag
-     * @return : The number of occurences of the item in the bag
+     * @return The number of occurences of the item in the bag
      */
     public int getFrequencyOf(T item);
 
     /**Determine if the specified item is in the bag.
      * 
      * @param item : The specified item
-     * @return:
-     * true : The specified item is in the bag
-     * false: The specified item is not in the bag
+     * @return true : The specified item is in the bag
      */
     public boolean contains(T item);
 
     /**Retrieves all of the items in the form of array
      * 
-     * @return: The array of the items in the bag
+     * @return The array of the items in the bag
     */
     public T[] toArray();
 
@@ -77,8 +71,8 @@ public interface BagInterface <T>{
      * Bag B : {5,6,7}
      * Bag A union Bag B : {1,2,3,4,5,6,7}
      * 
-     * @param anotherBag : Another bag.
-     * @return : The class of the united bags.
+     * @param anotherBag Another bag.
+     * @return The class of the united bags.
      */
     public BagInterface<T> union(BagInterface<T> anotherBag);
 
@@ -88,8 +82,8 @@ public interface BagInterface <T>{
      * Bag B: {2,4,5,6}
      * Bag A intersection Bag B: {2,4}
      * 
-     * @param anotherBag : Another bag.
-     * @return : The class of the bag with common items.
+     * @param anotherBag Another bag.
+     * @return The class of the bag with common items.
      */
     public BagInterface<T> intersection(BagInterface<T> anotherBag);
     
@@ -99,8 +93,8 @@ public interface BagInterface <T>{
      * Bag B: {3,4,5}
      * Bag A difference Bag B: {1,2}
      * 
-     * @param anotherBag : Another bag
-     * @return : The bag class with the differences of the two bags.
+     * @param anotherBag Another bag
+     * @return The bag class with the differences of the two bags.
      */
     public BagInterface<T> difference(BagInterface<T> anotherBag);
 
