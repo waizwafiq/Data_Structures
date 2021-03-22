d1a1e9fa363077328ef7080cdfef0c21a427712a
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-03-22 15:08:21 +0800

    Update StackInterface.java

--- Stack/StackInterface.java
+++ Stack/StackInterface.java
@@ -8,9 +8,19 @@
 
 public interface StackInterface<T> {
     
+    /**Pushes the data onto the stack.
+     * The pushed data must be on the top of the stack.
+     * 
+     * @param data The data to be pushed onto the stack.
+     */
     public void push(T data);
 
+    /**Popping out the data from the stack. 
+     * The popped data must be the one at the top of the stack to obey
+     * the Last-In-First-Out (LIFO) concept.
+     */
     public void pop();
+
 
     public boolean isFull();
 
