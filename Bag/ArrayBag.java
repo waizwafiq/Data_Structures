b2468e9e75c120c6fb56bfea64592a8c700c3e0e
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-03-24 17:02:19 +0800

    Update ArrayBag.java
    +Fixed remove(T);
    +Fixed intersection(BagInterface<T>);

--- Bag/ArrayBag.java
+++ Bag/ArrayBag.java
@@ -2,40 +2,65 @@ import java.util.Random;
 
 public class ArrayBag<T> implements BagInterface<T> {
     private T[] bag;
-    private static final int MAX_CAPACITY = 1000;
+    private final int DEFAULT_CAPACITY = 25;
+    private int numberOfEntries;
 
     public ArrayBag() {
         bag = (T[]) new Object[0];
+        numberOfEntries = 0;
+    }
+
+    //COPY METHOD 1
+    public ArrayBag(ArrayBag<T> anotherBag) {
+        this.bag = anotherBag.toArray();
+        this.numberOfEntries = bag.length;
+    }
+
+    //COPY METHOD 2
+    public ArrayBag(BagInterface<T> anotherBag) {
+        this.bag = anotherBag.toArray();
+        this.numberOfEntries = bag.length;
     }
 
     public ArrayBag(T[] bag) {
-        if (bag.length <= MAX_CAPACITY)
+        if (bag.length <= DEFAULT_CAPACITY) {
             this.bag = bag;
-        else
+            numberOfEntries = bag.length;
+        } else
             throw new ExceptionInInitializerError("The bag reached its maximum capacity.");
     }
 
+    @Override
     public int getCurrentSize() {
-        return bag.length;
+        return numberOfEntries;
     }
 
+    @Override
+    public boolean isFull() {
+        return numberOfEntries == DEFAULT_CAPACITY;
+    }
+
+    @Override
     public boolean isEmpty() {
-        return bag.length == 0;
+        return numberOfEntries == 0;
     }
 
+    @Override
     public boolean add(T newItem) {
-        if (bag.length <= MAX_CAPACITY) {
+        if (bag.length <= DEFAULT_CAPACITY) {
             T[] temp = (T[]) new Object[getCurrentSize() + 1];
             for (int i = 0; i < getCurrentSize(); i++)
                 temp[i] = bag[i];
             temp[getCurrentSize()] = newItem;
             bag = temp;
+            numberOfEntries = temp.length;
             return true;
         } else
             return false;
 
     }
 
+    @Override
     public T remove() {
         if (!isEmpty()) {
             T toRemove = bag[new Random().nextInt(getCurrentSize())];
@@ -46,28 +71,40 @@ public class ArrayBag<T> implements BagInterface<T> {
                     temp[j++] = bag[i];
 
             bag = temp;
+            numberOfEntries = temp.length;
             return toRemove;
         } else
             return null;
     }
 
+    @Override
     public boolean remove(T toRemove) {
         if (!isEmpty() && contains(toRemove)) {
+
+            boolean removed = false;
             T[] temp = (T[]) new Object[getCurrentSize() - 1];
+
             for (int i = 0, j = 0; i < getCurrentSize(); i++)
-                if (bag[i] != toRemove)
+                if (!bag[i].equals(toRemove) || removed)
                     temp[j++] = bag[i];
+                else if (bag[i].equals(toRemove))
+                    removed = true; //to avoid multiple removal
 
             bag = temp;
+            numberOfEntries = temp.length;
             return true;
+
         } else
             return false;
     }
 
+    @Override
     public void clear() {
         bag = (T[]) new Object[0];
+        numberOfEntries = 0;
     }
 
+    @Override
     public int getFrequencyOf(T item) {
         if (!isEmpty() && contains(item)) {
             int count = 0;
@@ -80,22 +117,28 @@ public class ArrayBag<T> implements BagInterface<T> {
             return 0;
     }
 
+    @Override
     public boolean contains(T item) {
-        if (!isEmpty())
+        if (!isEmpty()) {
             for (T items : bag)
                 if (items.equals(item))
                     return true;
-
+        }
         return false; // if not found
 
         // alternative: return (!isEmpty() && getIndexOf(item) != -1);
     }
 
     @Override
+    public T[] toArray() {
+        return bag;
+    }
+
+    @Override
     public BagInterface<T> union(BagInterface<T> anotherBag) {
         int len = getCurrentSize() + anotherBag.getCurrentSize();
 
-        if (len <= MAX_CAPACITY) {
+        if (len <= DEFAULT_CAPACITY) {
             ArrayBag<T> newBag = new ArrayBag<>();
             for (T item : bag)
                 newBag.add(item);
@@ -110,12 +153,20 @@ public class ArrayBag<T> implements BagInterface<T> {
     @Override
     public BagInterface<T> intersection(BagInterface<T> anotherBag) {
         if (!this.isEmpty() && !anotherBag.isEmpty()) {
-            ArrayBag<T> newBag = new ArrayBag<>();
-            for (T item1 : bag)
-                for (T item2 : anotherBag.toArray())
-                    if (item1.equals(item2))
-                        newBag.add(item1);
+            BagInterface<T> temp1 = new ArrayBag<>(this);
+            BagInterface<T> temp2 = new ArrayBag<>(anotherBag);
 
+            ArrayBag<T> newBag = new ArrayBag<>();
+
+            for (T item1 : temp1.toArray())
+                for (T item2 : temp2.toArray()) {
+                    if (item1.equals(item2)) {
+                        temp1.remove(item1);
+                        temp2.remove(item2);
+                        newBag.add(item1);
+                        break;
+                    }
+                }
             return newBag;
         } else
             return null;
@@ -126,11 +177,11 @@ public class ArrayBag<T> implements BagInterface<T> {
         if (!this.isEmpty() && !anotherBag.isEmpty()) {
             ArrayBag<T> newBag = new ArrayBag<>();
 
-            for (int i = 0; i < getCurrentSize(); i++) 
-                for (int j = 0; j < anotherBag.getCurrentSize(); j++) 
-                    if(bag[i].equals(anotherBag.toArray()[j]))
+            for (int i = 0; i < getCurrentSize(); i++)
+                for (int j = 0; j < anotherBag.getCurrentSize(); j++)
+                    if (bag[i].equals(anotherBag.toArray()[j]))
                         break;
-                    else if (j == anotherBag.getCurrentSize() - 1) 
+                    else if (j == anotherBag.getCurrentSize() - 1)
                         newBag.add(bag[i]);
 
             return newBag;
@@ -138,9 +189,5 @@ public class ArrayBag<T> implements BagInterface<T> {
             return this;
         else
             return null;
-    }
-
-    public T[] toArray() {
-        return bag;
     }
 }
