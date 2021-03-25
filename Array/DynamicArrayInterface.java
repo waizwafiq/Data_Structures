89d6582fd69048276b8e7310776af5b24e099716
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-03-25 13:13:38 +0800

    Added display(void);

--- Array/DynamicArrayInterface.java
+++ Array/DynamicArrayInterface.java
@@ -2,7 +2,7 @@
  * This is the interface for the dynamic array.
  * <p>
  * <p>
- * Reference: 
+ * Reference:
  * 
  * <p>
  * NOTE: I may make some corrections for this source code.
@@ -18,14 +18,14 @@ public interface DynamicArrayInterface<T> {
      * @return: The size of the dynamic array.
      */
     public int getCurrentSize();
-  
+
     /**
      * Check if the dynamic array is empty.
      *
      * @return true : The dynamic array is empty
      */
     public boolean isEmpty();
-  
+
     /**
      * Add an item into the dynamic array.
      *
@@ -36,17 +36,18 @@ public interface DynamicArrayInterface<T> {
 
     /**
      * Add an item into the dynamic array, given a specified index.
+     * 
      * @return true if successful
      */
     public boolean add(T newItem, int idx);
-  
+
     /**
      * Remove a random item from the dynamic array
      *
      * @return: The item removed from the dynamic array
      */
     public T remove();
-  
+
     /**
      * Removes a specified item from the dynamic array
      *
@@ -54,10 +55,10 @@ public interface DynamicArrayInterface<T> {
      * @return: true: item removal successful
      */
     public boolean remove(T item);
-  
+
     /** Removes all of the items from the dynamic array */
     public void clear();
-  
+
     /**
      * Get the number of occurrences of the item in the dynamic array
      *
@@ -68,11 +69,12 @@ public interface DynamicArrayInterface<T> {
 
     /**
      * Get the number of index of the specified element. (-1 if not found)
+     * 
      * @param item The specified element
      * @return The index number
      */
     public int getIndexOf(T item);
-  
+
     /**
      * Determine if the specified item is in the dynamic array.
      *
@@ -80,45 +82,46 @@ public interface DynamicArrayInterface<T> {
      * @return true : The specified item is in the dynamic array
      */
     public boolean contains(T item);
-  
+
     /**
      * Retrieves all of the items in the form of array
      *
      * @return The array of the items in the dynamic array
      */
     public T[] toArray();
-  
+
     /**
-     * The union of two arrays. Combines the elements inside two arrayss:
-     * Array A : {1,2,3,4}
-     * Array B : {5,6,7} 
-     * union : {1,2,3,4,5,6,7}
+     * The union of two arrays. Combines the elements inside two arrayss: Array A :
+     * {1,2,3,4} Array B : {5,6,7} union : {1,2,3,4,5,6,7}
      *
      * @param anotherArray Another dynamic array.
      * @return The class of the united arrays.
      */
     public DynamicArrayInterface<T> union(DynamicArrayInterface<T> anotherArray);
-  
+
     /**
-     * The intersection of two arrays. 
-     * A new dynamic array that has the common elements of those two arrays. 
-     * Array A: {1,2,3,4} Array B: {2,4,5,6} 
-     * Array A intersection Array B: {2,4}
+     * The intersection of two arrays. A new dynamic array that has the common
+     * elements of those two arrays. Array A: {1,2,3,4} Array B: {2,4,5,6} Array A
+     * intersection Array B: {2,4}
      *
      * @param anotherArray Another dynamic array.
      * @return The class of the dynamic array with common elements.
      */
     public DynamicArrayInterface<T> intersection(DynamicArrayInterface<T> anotherArray);
-  
+
     /**
-     * The difference of two arrays. 
-     * A new dynamic array that has the differences of those two arrays. 
-     * Array A: {1,2,3,4} Array B: {3,4,5} 
-     * Array A difference Array B: {1,2}
+     * The difference of two arrays. A new dynamic array that has the differences of
+     * those two arrays. Array A: {1,2,3,4} Array B: {3,4,5} Array A difference
+     * Array B: {1,2}
      *
      * @param anotherArray Another dynamic array
      * @return The dynamic array class with the differences of the two arrays.
      */
     public DynamicArrayInterface<T> difference(DynamicArrayInterface<T> anotherArray);
-  
-  }
+
+    /**
+     * Display elements in the dynamic array.
+     */
+    public void display();
+
+}
