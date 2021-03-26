b5282a1a680ed4902f908aa93dc5bb4ae8a3aa35
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-03-26 09:53:33 +0800

    Update LinkedBag.java

--- Bag/LinkedBag.java
+++ Bag/LinkedBag.java
@@ -1,1 +1,82 @@
+public final class LinkedBag<T> implements BagInterface<T> {
 
+    
+
+    @Override
+    public int getCurrentSize() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean add(T newItem) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public T remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean remove(T item) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int getFrequencyOf(T item) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean contains(T item) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public T[] toArray() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public BagInterface<T> union(BagInterface<T> anotherBag) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public BagInterface<T> intersection(BagInterface<T> anotherBag) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public BagInterface<T> difference(BagInterface<T> anotherBag) {
+        // TODO Auto-generated method stub
+        return null;
+    }   
+}
