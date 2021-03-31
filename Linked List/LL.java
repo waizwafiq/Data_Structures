e81abda0fef9a25b2c891727f63811d640d5f5ee
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-03-31 10:43:32 +0800

    Update LL.java

--- Linked List/LL.java
+++ Linked List/LL.java
@@ -1,144 +1,203 @@
-public class LL<T> {
-
-    private Node<T> head, tail;
-    private int size;
+public class LL<E> {
 
     private class Node<T> {
-        Node<T> next;
-        T data;
+        private Node<T> next;
+        private T element;
 
-        public Node(T data) {
+        public Node(T element) {
             this.next = null;
-            this.data = data;
+            this.element = element;
         }
 
-        public Node(Node next, T data) {
+        public Node(Node next, T element) {
             this.next = next;
-            this.data = data;
-        }
-
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
-
-        public T getData() {
-            return data;
-        }
-
-        public void setData(T data) {
-            this.data = data;
+            this.element = element;
         }
     }
+
+    private Node<E> head, tail;
+    private int size;
 
     public LL() {
     }
 
-    public void addFirst(T data) {
-        Node<T> newNode = new Node<>(data);
-        newNode.next = head;
-        head = newNode;
-        size++;
+    public void addFirst(E e) {
+        Node<E> newNode = new Node<>(e);
 
-        if (tail == null)
-            tail = head;
-    }
-
-    public void addLast(T data) {
-        Node<T> newNode = new Node<>(data);
-        if (tail == null)
-            head = tail = newNode;
+        if (head == null) //if the head is empty (list is empty)
+            head = tail = newNode; // the head is the tail and is the new node.
         else {
-            tail.next = newNode; // insert newNode into the new tail
-            tail = tail.next; // update tail value
+            newNode.next = head; //the node after the new one is the initial head
+            head = newNode; //now the head is the new node
         }
         size++;
     }
 
-    public void add(int idx, T data) {
-        if (idx == 0)
-            addFirst(data);
-        else if (idx >= size)
-            addLast(data);
-        else {
-            Node<T> current = head;
+    public void addLast(E e) {
+        Node<E> newNode = new Node<>(e);
 
-            for (int i = 1; i < idx; i++) // traverse and stop before requested idx
+        if (tail == null) { //if the tail is empty (list is empty)
+            head = tail = newNode; //the tail is the head and is the new node.
+        } else {
+            tail.next = newNode; //the node after the initial tail is the new node
+            tail = newNode; //now the new node is the tail.
+        }
+        size++;
+    }
+
+    public void add(int index, E e) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException("Negative index is not possible!");
+        } else if (index == 0) {
+            addFirst(e);
+        } else if (index >= size) {
+            addLast(e);
+        } else {
+            Node<E> current = head;
+            for (int i = 0; i < index - 1; i++) //traverse and stop before the requested index
                 current = current.next;
 
-            Node<T> temp = current.next; // the node at idx
-            current.next = new Node<>(data); // place node between current & temp
-            (current.next).next = temp; // place the temp (node at idx before) to the next newNode
+            Node<E> temp = current.next; //temporary for disconnection
+
+            current.next = new Node<>(e);
+            current.next.next = temp;
             size++;
         }
     }
 
-    public T removeFirst() {
+    public E removeFirst() {
         if (size == 0)
             return null;
         else {
-            Node<T> temp = head;
-            head = head.next; // replace head with the next node (2nd node)
-            size--;
-            if (head == null)
-                tail = null;
+            Node<E> temp = head;
 
-            return temp.data;
+            head = head.next;
+            size--;
+
+            return temp.element;
         }
     }
 
-    public T removeLast() {
-        if(size == 0)
+    public E removeLast() {
+        if (size == 0)
             return null;
-        else if (size == 1){
-            Node<T> temp = head;
+        else if (size == 1) {
+            Node<E> temp = head;
             head = tail = null;
-            size = 0;
+            size--;
+            return temp.element;
+        } else {
+            Node<E> temp = tail;
+            Node<E> current = head;
 
-            return temp.data;
-        }else{
-            Node<T> current = head;
-            
-            for(int i=0; i<size-2; i++)
+            for (int i = 0; i < size - 2; i++) //traverse until the node before the tail
                 current = current.next;
 
-            Node<T> temp = tail;
             tail = current;
             tail.next = null;
             size--;
 
-            return temp.data;
+            return temp.element;
         }
     }
 
-    public T remove(int idx){
-        if(idx < 0 || idx >= size) return null;
-        else if(idx == 0) return removeFirst();
-        else if(idx == size-1) return removeLast();
+    public E remove(int index) {
+        if (index < 0 || index >= size)
+            return null;
+        else if (index == 0)
+            return removeFirst();
+        else if (index == size - 1)
+            return removeLast();
         else {
-            Node<T> previous = head; //to get the node before the specified idx
-            
-            for(int i=1; i<idx; i++ )
-                previous = previous.next;
+            Node<E> current = head;
 
-            Node<T> temp = previous.next; //the node to be removed
-            previous.next = temp.next;
+            for (int i = 0; i < index - 1; i++) //traverse and stop before the requested index
+                current = current.next;
+
+            Node<E> temp = current.next;
+            current.next = temp.next;
             size--;
 
-            return temp.data;
+            return temp.element;
         }
     }
 
-    public void display() {
-        Node<T> current = head;
-        while (current != null) {
-            System.out.print(current.data + " -> ");
+    public void add(E e) {
+        if (size == 0)
+            addFirst(e);
+        else
+            addLast(e);
+    }
+
+    public boolean contains(E e) {
+        if (size == 0)
+            return false;
+        else {
+            Node<E> current = head;
+            while (current != null) {
+                if (current.element.equals(e))
+                    return true;
+                current = current.next;
+            }
+
+            return false;
+        }
+    }
+
+    public E get(int index) {
+        if (size == 0)
+            return null;
+        else {
+            Node<E> current = head;
+
+            for (int i = 0; i < index; i++) //traverse until the index
+                current = current.next;
+
+            return current.element;
+        }
+    }
+
+    public E getFirst() {
+        return head.element;
+    }
+
+    public E getLast() {
+        return tail.element;
+    }
+
+    public int indexOf(E e) {
+        Node<E> current = head; //starting from the head
+
+        for (int i = 0; i < size - 1; i++) {
+            if (current.element.equals(e))
+                return i;
             current = current.next;
         }
-        System.out.println("null");
+
+        return -1;
+    }
+
+    public int lastIndexOf(E e) {
+        /*note:
+         * I'm still looking for a better to do this method.
+         * Singly linked list cannot be traversed backwards, it can only go forward.
+         * This is a major disadvantage to get the lastIndexOf() since we cannot start
+         * from the tail of the linked list, then traverse it backwards.
+         */
+
+        Node<E> current = head;
+        int idx = -1;
+
+        for (int i = 0; i < size - 1; i++) {
+            if (current.element.equals(e))
+                idx = i;
+            current = current.next;
+        }
+        return idx;
+    }
+
+    public E set(int index, E e){
+
     }
 
 }
