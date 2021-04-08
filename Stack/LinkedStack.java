621338e60196b3152d20acf10980da3821f0edff
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-04-08 13:12:14 +0800

    Create LinkedStack.java

---
+++ Stack/LinkedStack.java
@@ -0,0 +1,46 @@
+import java.util.EmptyStackException;
+
+public class LinkedStack<T> implements StackInterface<T> {
+
+    private LL<T> list;
+
+    public LinkedStack() {
+        list = new LL<>();
+    }
+
+    @Override
+    public void push(T data) {
+        list.addFirst(data);
+    }
+
+    @Override
+    public T pop() {
+        if (isEmpty())
+            throw new EmptyStackException();
+
+        T popped = list.getFirst();
+        list.removeFirst();
+
+        return popped;
+    }
+
+    @Override
+    public T peek() {
+        return list.getFirst();
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
+
+    @Override
+    public boolean search(T o) {
+        return list.contains(o);
+    }
+}
