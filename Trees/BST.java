5c9adceb5293af706001a4e0f3d26fcb888ff595
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-04-19 13:30:11 +0800

    Added remove() and getPredecessor() methods

--- Trees/BST.java
+++ Trees/BST.java
@@ -34,6 +34,46 @@ public class BST<T extends Comparable<T>> {
 
         return current;
     }
+    
+    public boolean remove(T e) {
+        root = remove(root, e);
+        return found;
+    }
+
+    public Node<T> remove(Node<T> current, T e) {
+        if (current == null)
+            found = false;
+        else if (e.compareTo(current.element) < 0)
+            current.left = remove(current.left, e);
+        else if (e.compareTo(current.element) > 0)
+            current.right = add(current.right, e);
+        else {
+            current = removeNode(current);
+            found = true;
+        }
+
+        return current;
+    }
+
+    private Node<T> removeNode(Node<T> node) {
+        if (node.left == null)
+            return node.right;
+        else if (node.right == null)
+            return node.left;
+        else {
+            T e = getPredecessor(node.left);
+            node.element = e;
+            node.left = remove(node.left, e);
+            return node;
+        }
+    }
+
+    public T getPredecessor(Node<T> current) {
+        while (current.right != null)
+            current = current.right;
+
+        return current.element;
+    }
 
     public boolean isEmpty() {
         return root == null;
