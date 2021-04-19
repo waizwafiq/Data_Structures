910651a2c3d601b802176ff661c656cf1e3b8b7d
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-04-19 12:41:17 +0800

    Update BST.java

--- Trees/BST.java
+++ Trees/BST.java
@@ -1,1 +1,19 @@
 //BINARY SEARCH TREE
+public class BST<T extends Comparable<T>> {
+
+    private class Node<E> {
+        private Node<E> left, right;
+        private E element;
+
+        public Node(E element) {
+            this.element = element;
+            left = right = null;
+        }
+    }
+
+    private Node<T> root;
+
+    public BST(){
+        root = null;
+    }
+}
