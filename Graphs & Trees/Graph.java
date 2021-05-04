97ccc2c0582de4c73112a1a141b3077135979a77
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-05-04 09:43:42 +0800

    Update Graph.java

--- Graphs & Trees/Graph.java
+++ Graphs & Trees/Graph.java
@@ -1,10 +1,10 @@
 public class Graph<T extends Comparable<T>, N extends Comparable<N>> {
 
     private class Vertex<T extends Comparable<T>, N extends Comparable<N>> {
-        T vertexInfo;
-        int indeg, outdeg;
-        Vertex<T, N> nextVertex; // reference to next vertex/node
-        Edge<T, N> firstEdge; // reference to the first edge node
+        private T vertexInfo;
+        private int indeg, outdeg;
+        private Vertex<T, N> nextVertex; // reference to next vertex/node
+        private Edge<T, N> firstEdge; // reference to the first edge node
 
         public Vertex() {
             vertexInfo = null;
@@ -23,9 +23,9 @@ public class Graph<T extends Comparable<T>, N extends Comparable<N>> {
     }
 
     private class Edge<T extends Comparable<T>, N extends Comparable<N>> {
-        Vertex<T,N> toVertex;
-        N weight;
-        Edge<T,N> nextEdge;
+        private Vertex<T, N> toVertex;
+        private N weight;
+        private Edge<T, N> nextEdge;
 
         public Edge() {
             toVertex = null;
@@ -33,10 +33,87 @@ public class Graph<T extends Comparable<T>, N extends Comparable<N>> {
             nextEdge = null;
         }
 
-        public Edge(Vertex<T,N> destination, N weight, Edge<T,N> nextEdge){
+        public Edge(Vertex<T, N> destination, N weight, Edge<T, N> nextEdge) {
             this.toVertex = destination;
             this.weight = weight;
             this.nextEdge = nextEdge;
         }
+    }
+
+    private Vertex<T, N> head;
+    private int size;
+
+    public Graph() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        // get the number of vertices in the graph
+        return size;
+    }
+
+    public boolean hasVertex(T vertexInfo) {
+        if (head == null) // if the graph is empty
+            return false;
+
+        Vertex<T, N> temp = head; // starting from the head of the graph
+        while (temp != null) { // traverse until the final vertex
+            if (temp.vertexInfo.compareTo(vertexInfo) == 0)
+                return true; // but if the vertex is found, return true
+
+            temp = temp.nextVertex;
+        }
+        return false;
+    }
+
+    public int getInDeg(T vertexInfo) {
+        if (hasVertex(vertexInfo)) {
+            Vertex<T, N> temp = head; // starting from the head of the graph
+
+            while (temp != null) { // traverse until the final vertex
+                if (temp.vertexInfo.compareTo(vertexInfo) == 0)
+                    return temp.indeg; // if the vertex is found, return the indeg value;
+
+                temp = temp.nextVertex;
+            }
+        }
+        return -1; // not found
+    }
+
+    public int getOutDeg(T vertexInfo) {
+        if (hasVertex(vertexInfo)) {
+            Vertex<T, N> temp = head; // starting from the head of the graph
+
+            while (temp != null) { // traverse until the final vertex
+                if (temp.vertexInfo.compareTo(vertexInfo) == 0)
+                    return temp.outdeg; // if the vertex is found, return the outdeg value;
+
+                temp = temp.nextVertex;
+            }
+        }
+        return -1; // not found
+    }
+
+    public boolean addVertex(T vertexInfo) {
+        if (!hasVertex(vertexInfo)) {
+            Vertex<T, N> temp = head; // starting from head vertex
+
+            Vertex<T, N> newVertex = new Vertex<>(vertexInfo, null);
+
+            if (head == null) {
+                // if the graph is empty, the new vertex is the head of the graph
+                head = newVertex;
+            } else {
+                // if the graph is not empty
+                while (temp.nextVertex != null) // traverse until temp is the final vertex
+                    temp = temp.nextVertex;
+
+                temp.nextVertex = newVertex; // add the new vertex next to the final vertex
+            }
+            size++; // increase the number of vertices in the graph
+            return true;
+        } else
+            return false;
     }
 }
