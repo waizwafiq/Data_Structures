62268222f2cd08c053f90c4cb3e8d357cc0ce1c3
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-05-04 09:18:04 +0800

    Update Graph.java

--- Graphs & Trees/Graph.java
+++ Graphs & Trees/Graph.java
@@ -23,6 +23,20 @@ public class Graph<T extends Comparable<T>, N extends Comparable<N>> {
     }
 
     private class Edge<T extends Comparable<T>, N extends Comparable<N>> {
+        Vertex<T,N> toVertex;
+        N weight;
+        Edge<T,N> nextEdge;
 
+        public Edge() {
+            toVertex = null;
+            weight = null;
+            nextEdge = null;
+        }
+
+        public Edge(Vertex<T,N> destination, N weight, Edge<T,N> nextEdge){
+            this.toVertex = destination;
+            this.weight = weight;
+            this.nextEdge = nextEdge;
+        }
     }
 }
